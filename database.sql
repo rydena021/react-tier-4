154452dc06a28c66ebd06ad1e088d4f2f9befe2e
Anders Ryden <rydena021@gmail.com>
2019-01-03 14:53:45 -0600

    connect to DB

--- database.sql
+++ database.sql
@@ -1,5 +1,13 @@
 CREATE TABLE "person" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "first_name" VARCHAR (240),
+    "last_name" VARCHAR (240),
+    "email" VARCHAR (240),
+    "avatar_url" VARCHAR (1000),
+    "notifications" BOOLEAN DEFAULT FALSE,
+    "application_goal" INTEGER DEFAULT 10,
+    "commit_goal" INTEGER DEFAULT 5,
+    "meetup_goal" INTEGER DEFAULT 1
 );
